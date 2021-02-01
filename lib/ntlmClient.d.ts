dbbaea044744e96153c1e25466e8375ee0040d0d
CatButtes <catbuttes@outlook.com>
2021-02-01 15:52:11 +0000

    Remove built files from git

--- lib/ntlmClient.d.ts
+++
@@ -1,3 +0,0 @@
-import { AxiosInstance } from 'axios';
-export declare function NtlmClient(username: string, password: string, domain: string, workstation?: string, AxiosClient?: AxiosInstance): AxiosInstance;
-//# sourceMappingURL=ntlmClient.d.ts.map
